bad7a9530faf8797324f7c17f0a0291a058be006
Aditya Parab <contact.adityaparab@gmail.com>
2019-02-06 03:47:12 +0100

    updated footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import { createStyles, withStyles } from '@material-ui/core/styles';
 import { Typography } from '@material-ui/core';
 
+import CodeIcon from '@material-ui/icons/Code';
+
 const styles = createStyles({
     footer: {
         backgroundColor: '#292c2f',
@@ -13,25 +15,43 @@ const styles = createStyles({
     },
     link: {
         color: 'white',
-        display: 'inline-block',
-        margin: '0 5px'
+        margin: '0 5px',
+        fontWeight: 200
+    },
+    iconL: {
+        color: 'white',
+        fontWeight: 400,
+        margin: '0 5px',
+        fontSize: '32px'
+    },
+    spacer: {
+        flex: 1,
+        flexGrow: 1
+    },
+    padded: {
+        padding: '0 10px'
     }
 })
 
 function Footer(props: any) {
-    const { footer, link } = props.classes;
+    const { footer, link, padded, spacer, iconL } = props.classes;
     return (
         <footer className={footer}>
-            <div>
-                <Typography className={link} component="span">
-                    Created By
-                    <a href="https://github.com/adityaparab" target="_blank">
-                        <Typography component="span" className={link}> 
-                            Aditya Parab
-                        </Typography>
-                    </a>
-                </Typography>
-            </div> 
+            <div className={padded}>
+                <Typography className={link}>Created by,</Typography>
+                <a href="https://github.com/adityaparab" target="_blank">
+                    <Typography component="h6" className={link}>
+                        Aditya Parab
+                    </Typography>
+                </a>
+                <Typography className={link} component="h6">contact.adityaparab@gmail.com</Typography>
+            </div>
+            <div className={spacer} />
+            <div className={padded}>
+                <a href="https://github.com/adityaparab/redux-ui-builder">
+                    <CodeIcon className={iconL} />
+                </a>
+            </div>
         </footer>
     );
 }
